2a6eeba582b2e5e50f4f396cbec71ce6667c0d6b
Harsh Raj Singh <135263337+Harshrajput73@users.noreply.github.com>
2024-01-19 00:49:21 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,8 +6,8 @@ function Header() {
     <div className="border-teal-600 krishna flex px-10 bg-black border-4 p-3 justify-between">
       <div className="flex  max-[600px]:ml-9  ">
         <GiBookAura className="text-teal-600 text-4xl relative top-[1px]" />
-        <h1 className="text-white ml-2 text-3xl font-bold text-white">Saroj</h1>
-        <h1 className="text-teal-600 ml-2 font-bold text-3xl">Kumar</h1>
+        <h1 className="text-white ml-2 text-3xl font-bold text-white">Harsh Raj</h1>
+        <h1 className="text-teal-600 ml-2 font-bold text-3xl">Singh</h1>
       </div>
 
       <div className="max-[600px]:hidden">
